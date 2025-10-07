5d4b760abddec54083e204e66c769858c258bfeb
Claudia <claudia_4bcn@hotmail.com>
2025-10-07 17:56:43 +0200

    Level1-excercises1-4

---
+++ 1.4Rest-SpreadOperators.js
@@ -0,0 +1,16 @@
+// 1.4 Rest & Spread operators
+console.log("Excercise 4.1");
+
+const array1 = [0,1,2,3,4,5]
+const array2 = [6,7,8]
+
+console.log ("New array: " + [...array1,...array2])
+
+console.log("Excercise 4.1");
+
+//Exercici 1
+// Operador Spread en Arrays: Crea dues arrays, array1 i array2. 
+// Utilitza l'operador spread per a crear una tercera array que contingui tots els elements de array1 i array2.
+
+// Exercici 2
+// Operador Rest en Funcions: Crea una funció 'suma' que utilitzi l'operador rest per a acceptar un nombre indeterminat d'arguments i retornar la seva suma.
---
+++ 1.6ArrayLoops.js
@@ -0,0 +1,16 @@
+
+// 1.6 Array loops
+
+console.log("Excercise 6.1");
+// Exercici 1
+// forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+
+
+console.log("Excercise 6.2");
+// Exercici 2
+// for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+
+
+console.log("Excercise 6.3");
+// Exercici 3
+// filter: Teniu una array de números. Utilitza filter per a crear una nova array que només contingui els números parells. let numeros = [1, 2, 3, 4, 5, 6];
