e6ed51d5a0d34c1c6cb14fb74f58f530c9016bb0
Claudia <claudia_4bcn@hotmail.com>
2025-10-08 12:33:44 +0200

    Starting level2

--- 1.1ArrowFunctions.js
+++ 1.1ArrowFunctions.js
@@ -1,32 +1,16 @@
 // 1.1 ARROW FUNCTIONS.
-console.log("Excercise 1.1");
-function add(a, b) {
-  return a + b;
+console.log("Excercise 1.4");
+
+function printNumbers(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        const print = (num) => console.log(num);
+        print(numbers[i]);
+    }
 }
-console.log("Normal add function: " + add(3, 3));
-let addArrow = (a, b) => a + b;
-console.log("Arrow add function: " + addArrow(10, 15));
+
+const nums = [1, 2, 3, 4, 5];
+printNumbers(nums);
 
 
-console.log("Excercise 1.2");
-function random() {
-  return Math.floor(Math.random() * (100 - 0 + 1) + 0);
-}
-console.log("Normal random number function: " + random());
-let randomArrow = () => Math.floor(Math.random() * (100 - 0 + 1) + 0);
-console.log("Arrow random number function: " + randomArrow());
-
-
-console.log("Excercise 1.3");
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-  greet() {
-    let arrowFunction = () =>
-      console.log(`Greeting arrow function: Hola, ${this.name}`);
-    return arrowFunction();
-  }
-}
-let person1 = new Person("Claudia");
-person1.greet();
+// Exercici 4
+// Funció de fletxa dins d'un loop: Crea una funció anomenada printNumbers que accepti un array de números i utilitzi un loop for per imprimir cada número a la consola utilitzant una funció de fletxa.
--- 1.6ArrayLoops.js
+++ 1.6ArrayLoops.js
@@ -2,25 +2,3 @@
 // 1.6 Array loops
 
 console.log("Excercise 6.1");
-let names= ['Anna', 'Bernat', 'Clara'];
-names.forEach(names=> console.log(names));
-
-
-console.log("Excercise 6.2");
-let names2= ['Anna', 'Bernat', 'Clara'];
-for (let name of names2){
-    console.log(name);
-}
-
-console.log("Excercise 6.3");
-let numbers= [1, 2, 3, 4, 5, 6];
-let evenNumbers = numbers.filter(num => num % 2 === 0);
-console.log(evenNumbers);   
-
-// Exercici 1
-// forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
-// Exercici 2
-// for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
-// Exercici 3
-// filter: Teniu una array de números. Utilitza filter per a crear una nova array que només contingui els números parells. let numeros = [1, 2, 3, 4, 5, 6];
-
