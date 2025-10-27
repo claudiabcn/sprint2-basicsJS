d196c7630ec0a43e44b4e68933435f8a8de231f1
Claudia <claudia_4bcn@hotmail.com>
2025-10-27 13:27:10 +0100

    Add upperString function and add spread functions withe sum of objects

--- 1.3Callbacks.js
+++ 1.3Callbacks.js
@@ -1,16 +1,9 @@
-// 1.3 CALLBACKS.
-console.log("Excercise 3.3");
-function esperarISaludar(nom, callback) {
-    setTimeout(() => {
-        callback(nom);
-    }, 2000);
+console.log("Exercise 3.4");
+function processString(string, callback) {
+  const upperString = string.toUpperCase();
+  callback(upperString);
 }
 
-function saludar(nom) {
-    console.log(`Hola, ${nom}!`);
-}
-
-esperarISaludar("Anna", saludar);
-
-
-// Ús de callbacks en funcions asíncrones: Escriu una funció esperarISaludar que accepti dos paràmetres: un nom i una funció de callback. La funció ha d'esperar 2 segons i llavors invocar la funció de callback, passant el nom com a paràmetre.
+processString('hello Claudia', function(result) {
+  console.log(result);
+});
--- 1.4Rest-SpreadOperators.js
+++ 1.4Rest-SpreadOperators.js
@@ -1,17 +1,12 @@
-// 1.4 Rest & Spread operators
-console.log("Excercise 4.3");
-
-function copiarObjecte(obj) {
-    const objCopia = { ...obj };
-    return objCopia;
+console.log("Excercise 4.5");
+function myFunction(a, b, c) {
+console.log(a,b,c)
 }
+const values = [1, 2, 3];
+myFunction(...values);
 
-const objecte1 = { a: 1, b: 2, c: 3 };
-const objecte2 = copiarObjecte(objecte1);
-objecte2.b = 20;
-
-console.log("Objecte 1:", objecte1); 
-console.log("Objecte 2:", objecte2); 
-
-
-// Copiant objectes amb Spread: Crea un objecte 'objecte1'. Després crea un segon objecte, 'objecte2', que sigui una còpia de 'objecte1' utilitzant l'operador spread. Canvia una propietat de 'objecte2' i comprova que 'objecte1' no ha canviat.
+console.log("Excercise 4.6");
+const person = { name: "Claudia", age: 31 }
+const job = { role: "Developer", company: "Amazon" }
+const merged = { ...person, ...job }
+console.log(merged);
